9656f0f584871a5b0b52599b8587c3eb59ad2fa7
Shantel <Shantelgray24@gmail.com>
2025-06-26 02:50:55 -0400

    Sushi Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([]);
+  const [eatenIds, setEatenIds] = useState([]);
+  const startMoney = 80;
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+      .then((res) => res.json())
+      .then(setSushis);
+  }, []);
+
+  const moneySpent = eatenIds.reduce((total, id) => {
+    const sushiEaten = sushis.find((sushi) => sushi.id === id);
+    return sushiEaten ? total + sushiEaten.price : total;
+  }, 0);
+
+  const moneyLeft = startMoney - moneySpent;
+
+  function handleEaten(sushi) {
+    if (eatenIds.includes(sushi.id)) return;
+    if (sushi.price <= moneyLeft) {
+      setEatenIds([...eatenIds, sushi.id]);
+    } else {
+      alert("Brokie");
+    }
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer
+        sushis={sushis}
+        eatenIds={eatenIds}
+        handleEaten={handleEaten}
+        setSushis={setSushis}
+        moneyLeft={moneyLeft}
+      />
+      <Table plates={eatenIds} remainingBudget={moneyLeft} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({
+  sushis,
+  setSushis,
+  handleEaten,
+  eatenIds,
+  moneyLeft,
+}) {
+  const [currentSushi, setCurrentSushi] = useState(0);
+  const sliceSize = 4;
+
+  function handleClick() {
+    setCurrentSushi((currentSushi) => currentSushi + sliceSize);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+      .then((res) => res.json())
+      .then((data) => setSushis(data));
+  }, []);
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.slice(currentSushi, sliceSize + currentSushi).map((sushi) => (
+        <Sushi
+          sushi={sushi}
+          key={sushi.id}
+          setSushis={setSushis}
+          isEaten={eatenIds.includes(sushi.id)}
+          onEat={handleEaten}
+          moneyLeft={moneyLeft}
+        />
+      ))}
+      <MoreButton handleClick={handleClick} />
     </div>
   );
 }
